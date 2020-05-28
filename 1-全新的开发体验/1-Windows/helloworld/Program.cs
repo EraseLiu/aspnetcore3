dc79b978c8cccd63c160fea251980f98e118556c
liu_jinxiang <erase_liu@yahoo.com>
2020-05-28 10:58:22 +0800

    01. 显示制定 URL

--- 1-全新的开发体验/1-Windows/helloworld/Program.cs
+++ 1-全新的开发体验/1-Windows/helloworld/Program.cs
@@ -12,6 +12,7 @@ namespace helloworld
             Host.CreateDefaultBuilder()
                 .ConfigureWebHost(webHostBuilder => webHostBuilder
                     .UseKestrel()
+                    .UseUrls("http://0.0.0.0:3721", "https://0.0.0.0:9527")
                     .Configure(applicationBuilder => applicationBuilder.Run(
                         context => context.Response.WriteAsync("Hello world!"))))
                 .Build()
